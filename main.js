2cb4bf07fd927bfefd8c0ee1619c64d3fa3968b7
root <root@KRISTY-PC.localdomain>
2021-05-04 16:56:32 +0800

    final version

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 let button = document.getElementById ('button');
 let number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 let passcode = document.querySelector('.passcode');
+let history_board = document.querySelector('.passcode-history');
 
 button.addEventListener ('click', generateNumber);
 
@@ -27,9 +28,13 @@ function generateNumber() {
 
   passcode.innerHTML = sixDigits.split('').join(' ')
 
-  // console.log(sixDigits)
+  console.log(sixDigits)
+    }
 
-  }
+
+  getRecord(sixDigits) //
+}
+
 
   //alternative is to add another function
 
@@ -38,5 +43,26 @@ function generateNumber() {
   //
   // }
   //
+
+function getRecord(sixDigits) {
+  let element = document.createElement('div')
+  element.classList.add('passcode-record')
+
+//shows list of generated passcodes with remove button
+  element.innerHTML = `<p class ='record'>${sixDigits}</p>
+                      <button class='remove-button' type="button" name="button">Delete</button>`
+
+  history_board.appendChild(element)
+  let remove_button = element.querySelector('.remove-button');
+  remove_button.addEventListener('click',removeRecord)
+
+  console.log(history_board)
+  console.log(remove_button)
+
+}
+
+function removeRecord(e) {
+  let record = e.currentTarget.parentElement
+  history_board.removeChild(record)
 
 }
